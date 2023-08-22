eb52d2117b3264e95570437ea1dbc98317e6e0e2
Chen-Pang He <chen.pang.he@jdh8.org>
2023-08-23 05:41:49 +0800

    Propagate errors with ? to fail gracefully

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use local_ip_address::local_ipv6;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let uri = format!(
         "https://api.gandi.net/v5/livedns/domains/{}/records/{}/AAAA",
-        var("FQDN").unwrap(),
-        var("NAME").unwrap(),
+        var("FQDN")?,
+        var("NAME")?,
     );
 
     reqwest::Client::new()
         .put(uri)
-        .header("Authorization", "Apikey ".to_owned() + &dotenv::var("APIKEY").unwrap())
+        .header("Authorization", "Apikey ".to_owned() + &var("APIKEY")?)
         .header("Content-Type", "application/json")
-        .body(format!(r#"{{"rrset_values": ["{}"]}}"#, local_ipv6().unwrap()))
+        .body(format!(r#"{{"rrset_values": ["{}"]}}"#, local_ipv6()?))
         .send()
         .await?
         .error_for_status()?;
